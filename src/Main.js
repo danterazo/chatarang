fd7a9263ecd0a97dc31b2638604037fa6b6d6d06
Dante Razo <dante@razonet.org>
2018-07-03 10:00:15 -0400

    Add style to Main

--- src/Main.js
+++ src/Main.js
@@ -6,12 +6,19 @@ import Chat from './Chat'
 class Main extends Component{
 	render(){
 		return (
-			<div className="Main">
-				<Sidebar />
-				<Chat />
+			<div className="Main" style={styles}>
+				<Sidebar/>
+				<Chat/>
 			</div>
 		)
 	}
+}
+
+// Styling in JavaScript
+const styles = {
+	display: 'flex',
+	alignItems: 'stretch',
+	height: '100vh', // 'vh' percentage of height of window
 }
 
 export default Main
