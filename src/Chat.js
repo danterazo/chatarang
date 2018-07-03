94c7725adffb126d87d84aac6bc9709a78b46807
Dante Razo <dante@razonet.org>
2018-07-03 11:04:25 -0400

    Add messages to Chat's state

--- src/Chat.js
+++ src/Chat.js
@@ -2,13 +2,25 @@ import React, {Component} from 'react'
 
 import ChatHeader from './ChatHeader';
 import MessageList from './MessageList';
+import MessageForm from './MessageForm'
 
 class Chat extends Component{
+	constructor(){
+		super()
+		this.state = {
+			messages: [
+				{id: 1, userName: 'drazo', body: 'hello!!'},
+				{id: 2, userName: 'dstrus', body: 'hello!'},
+			]
+		}
+	}
+
 	render(){
 		return (
 			<div className="Chat">
 				<ChatHeader/>
-				<MessageList/>
+				<MessageList messages={this.state.messages}/>
+				<MessageForm/>
 			</div>
 		)
 	}
--- src/MessageList.js
+++ src/MessageList.js
@@ -3,15 +3,14 @@ import React from 'react'
 import Message from './Message'
 
 // stateless functional component
-const MessageList = () =>{
-	const messages = [
-		{id: 1, userName: 'drazo', body: 'hello!'},
-		{id: 2, userName: 'dstrus', body: 'hello!'},
-	]
-
+const MessageList = (props) =>{
 	return (
 		<div className="MessageList">
-			{messages.map(msg => <Message message={msg} key={msg.id}/>)}
+			{
+				props.messages.map(msg => (
+					<Message message={msg} key={msg.id}/>
+				))
+			}
 		</div>
 	)
 }
