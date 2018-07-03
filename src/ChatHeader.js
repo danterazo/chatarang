db7c716b5a0ac076e7c7b205cda93fb929efb792
Dante Razo <dante@razonet.org>
2018-07-03 16:35:33 -0400

    Add style to ChatHeader component

--- src/ChatHeader.js
+++ src/ChatHeader.js
@@ -3,13 +3,34 @@ import React from 'react'
 // stateless functional component
 const ChatHeader = () =>{
 	return (
-		<header className="ChatHeader">
-			<div className="roomInfo">
-				<h2>#general</h2>
+		<header className="ChatHeader" style={style1}>
+			<div className="roomInfo" style={style2}>
+				<h2 style={style3}>#general</h2>
 				<p>Announcements and general chat</p>
 			</div>
 		</header>
 	)
+}
+
+const style1 = {
+	backgroundColor: "#f3f3f3",
+	borderBottom: "1px solid #ccc",
+	height: "3rem",
+	padding: "0 1rem",
+	display: "flex",
+	alignItems: "center",
+
+}
+
+const style2 = {
+	fontSize: "1.1rem",
+	margin: "0",
+}
+
+const style3 = {
+	color: "#999",
+	margin: "0",
+	fontSize: "0.8rem",
 }
 
 // NOTE: if state isn't needed, make a stateless functional component
