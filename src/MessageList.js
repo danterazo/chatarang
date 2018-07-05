b4082a488e44e20cd2381d35a545b1760d0f7424
Dante Razo <dante@razonet.org>
2018-07-05 10:19:53 -0400

    Style MessageList component

--- src/MessageList.js
+++ src/MessageList.js
@@ -2,10 +2,22 @@ import React from 'react'
 
 import Message from './Message'
 
-// stateless functional component
 const MessageList = (props) =>{
 	return (
-		<div className="MessageList" style={styles.messageList}>
+		<div
+			className="MessageList"
+			style={styles.messageList}
+		>
+			<div
+				className="roomAnnouncement"
+				style={styles.roomAnnouncement}
+			>
+				<h3 style={styles.h3}>
+					#general
+				</h3>
+				<p>This is the very beginning of the #general room.</p>
+			</div>
+
 			{
 				props.messages.map(msg => (
 					<Message message={msg} key={msg.id}/>
@@ -17,21 +29,19 @@ const MessageList = (props) =>{
 
 const styles = {
 	messageList: {
-		backgroundColor: "white",
+		backgroundColor: 'white',
 		flex: 1,
-		paddingBottom: "1rem",
-		overflowY: "scroll",
-	}
-}
+		paddingBottom: '1rem',
+		overflowY: 'scroll',
+	},
 
-/* Leftover CSS:
-.MessageList .roomAnnouncement {
-  padding: 2rem 1rem;
-}
+	roomAnnouncement: {
+		padding: '2rem 1rem',
+	},
 
-.MessageList .roomAnnouncement h3 {
-  font-size: 1.5rem;
+	h3: {
+		fontSize: '1.5rem',
+	},
 }
- */
 
 export default MessageList
