e71090a067a36b592147f4fa5708f4d6e945dc0f
Dante Razo <dante@razonet.org>
2018-07-05 09:18:26 -0400

    Add and style Avatar component

---
+++ src/Avatar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Avatar = (props) =>{
+	return (
+		<div className="Avatar" style={styles}>placeholder</div>
+	)
+}
+
+const styles = {
+	height: "40px",
+	width: "40px",
+	fontSize: "1rem",
+	borderRadius: "20px",
+	marginRight: "0.5rem",
+}
+
+
+export default Avatar
