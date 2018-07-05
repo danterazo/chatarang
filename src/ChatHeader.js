9c1839d72f13bec2ceef1553221d60e6e8ca1f0d
Dante Razo <dante@razonet.org>
2018-07-05 10:14:17 -0400

    Style ChatHeader component

--- src/ChatHeader.js
+++ src/ChatHeader.js
@@ -1,38 +1,42 @@
 import React from 'react'
 
-// stateless functional component
 const ChatHeader = () =>{
 	return (
-		<header className="ChatHeader" style={style1}>
-			<div className="roomInfo" style={style2}>
-				<h2 style={style3}>#general</h2>
-				<p>Announcements and general chat</p>
+		<header
+			className="ChatHeader"
+			style={styles.chatHeader}
+		>
+			<div className="roomInfo">
+				<h2 style={styles.h2}>
+					#general
+				</h2>
+				<p style={styles.p}>
+					Announcements and general chat
+				</p>
 			</div>
 		</header>
 	)
 }
 
-const style1 = {
-	backgroundColor: "#f3f3f3",
-	borderBottom: "1px solid #ccc",
-	height: "3rem",
-	padding: "0 1rem",
-	display: "flex",
-	alignItems: "center",
+const styles = {
+	chatHeader: {
+		backgroundColor: '#f3f3f3',
+		borderBottom: '1px solid #ccc',
+		padding: '0.5rem 1rem',
+		display: 'flex',
+		alignItems: 'center',
+	},
 
+	h2: {
+		fontSize: '1.1rem',
+		margin: 0,
+	},
+
+	p: {
+		color: '#999',
+		margin: 0,
+		fontSize: '0.8rem',
+	}
 }
-
-const style2 = {
-	fontSize: "1.1rem",
-	margin: "0",
-}
-
-const style3 = {
-	color: "#999",
-	margin: "0",
-	fontSize: "0.8rem",
-}
-
-// NOTE: if state isn't needed, make a stateless functional component
 
 export default ChatHeader
