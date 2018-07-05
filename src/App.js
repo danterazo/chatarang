06f3e7c204618df2b7efcf13b0fee52e92a46383
Dante Razo <dante@razonet.org>
2018-07-05 11:33:37 -0400

    Implement authentication system

--- src/App.js
+++ src/App.js
@@ -2,24 +2,37 @@ import React, {Component} from 'react'
 
 import './App.css'
 import Main from './Main'
+import SignIn from './SignIn'
 
-class App extends Component{
+class App extends Component {
 	constructor(){
 		super()
 
 		this.state = {
-			user: {
-				uid: '12345',
-				displayName: 'Dante',
-				email: 'dante@email.com',
-			}
+			user: {},
 		}
+	}
+
+	handleAuth = (user) =>{
+		this.setState({user})
+	}
+
+	signedIn = () =>{
+		return this.state.user.uid
+	}
+
+	signOut = () =>{
+		this.setState({user: {}})
 	}
 
 	render(){
 		return (
 			<div className="App">
-				<Main user={this.state.user}/>
+				{
+					this.signedIn() // ternary
+						? <Main user={this.state.user} signOut={this.signOut}/>
+						: <SignIn handleAuth={this.handleAuth}/>
+				}
 			</div>
 		)
 	}
--- src/SignIn.js
+++ src/SignIn.js
@@ -1,21 +1,37 @@
 import React, {Component} from 'react'
 
-class SignIn extends Component{
+class SignIn extends Component {
 	state = {
 		email: '',
 	}
 
-	// see React documentation
 	handleChange = (ev) =>{
 		this.setState({email: ev.target.value})
+	}
+
+	handleSubmit = (ev) =>{
+		ev.preventDefault()
+		this.props.handleAuth({
+			uid: `${this.state.email}-ksdfjhu32472398`,
+			displayName: this.state.email,
+			email: this.state.email,
+		})
 	}
 
 	render(){
 		return (
 			<div className="SignIn">
-				<form>
-					<input autoFocus type="email" name="email" placeholder="Enter your email"
-						   value={this.state.email} onChange={this.handleChange}></input>
+				<form onSubmit={this.handleSubmit}>
+					<input
+						autoFocus
+						type="email"
+						name="email"
+						placeholder="Enter your email"
+						value={this.state.email}
+						onChange={this.handleChange}/>
+					<button type="submit">
+						Sign In
+					</button>
 				</form>
 			</div>
 		)
