5fa7b300a730ea37c0047812fabf161cad30b6ab
Dante Razo <dante@razonet.org>
2018-07-05 10:06:37 -0400

    Add RoomList component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,6 @@
 import React from 'react'
+
+import RoomList from './RoomList'
 /* ORIGINAL JAVASCRIPT:
 const Sidebar = (props) =>{
 	return (
@@ -23,13 +25,7 @@ const Sidebar = (props) =>{
 			<h1 style={styles.h1}>
 				XTBC 18
 			</h1>
-			<nav className="RoomList" style={styles.children}>
-				<h2>Rooms</h2>
-				<ul>
-					<li><a href="#">general</a></li>
-					<li><a href="#">random</a></li>
-				</ul>
-			</nav>
+			<RoomList/>
 		</aside>
 	)
 }
@@ -43,10 +39,6 @@ const styles = {
 		padding: '1rem 0',
 		display: 'flex',
 		flexDirection: 'column',
-	},
-
-	children: {
-		padding: '0 1rem',
 	},
 
 	h1: {
