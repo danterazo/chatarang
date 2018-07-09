4afeb64da574cefbf2278b035b8b1911ae2711f6
Dante Razo <dante@razonet.org>
2018-07-09 10:39:00 -0400

    Use icon from Google account

--- src/Avatar.js
+++ src/Avatar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Avatar = ({user}) =>{
-	const imageUrl = `https://api.adorable.io/avatars/40/${user.email}.png`
+	const imageUrl = user.photoUrl || `https://api.adorable.io/avatars/40/${user.email}.png`
 
 	return (
 		<div className="Avatar" style={{...styles, backgroundImage: `url(${imageUrl})`,}}></div>
@@ -14,6 +14,7 @@ const styles = {
 	fontSize: "1rem",
 	borderRadius: "20px",
 	marginRight: "0.5rem",
+	backgroundSize: '40px 40px',
 }
 
 export default Avatar
