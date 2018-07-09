56ade43a3f5b1795eef788bdad3948db89fb56a9
Dante Razo <dante@razonet.org>
2018-07-09 09:44:28 -0400

    Add logout button, remove unneeded comments

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,36 +1,18 @@
 import React from 'react'
 
+import UserInfo from './UserInfo'
 import RoomList from './RoomList'
-/* ORIGINAL JAVASCRIPT:
-const Sidebar = (props) =>{
-	return (
-		<div className="Sidebar">
-			{props.user.displayName}
-		</div>
-	)
-}
-*/
 
-// DAVEY JAVASCRIPT:
-const Sidebar = (props) =>{
+const Sidebar = ({user, signOut}) =>{
 	return (
 		<aside className="Sidebar" style={styles.sidebar}>
-			<div className="UserInfo" style={styles.children}>
-				<div className="user">
-					{props.user.displayName}
-				</div>
-				<a href="#">
-				</a>
-			</div>
-			<h1 style={styles.h1}>
-				XTBC 18
-			</h1>
+			<UserInfo user={user} signOut={signOut}/>
+			<h1 style={styles.h1}>XTBC 18</h1>
 			<RoomList/>
 		</aside>
 	)
 }
 
-// TODO: fix link style
 const styles = {
 	sidebar: {
 		backgroundColor: '#333344',
@@ -46,7 +28,7 @@ const styles = {
 		fontSize: '1.2rem',
 		marginTop: 0,
 		padding: '0 1rem',
-	}
+	},
 }
 
 export default Sidebar
