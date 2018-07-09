71225742ec887785fd23932e8fda4a8a63888e8b
Dante Razo <dante@razonet.org>
2018-07-09 10:39:36 -0400

    Use Google authentication

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {auth} from './base'
 
 import './App.css'
 import Main from './Main'
@@ -13,7 +14,26 @@ class App extends Component {
 		}
 	}
 
-	handleAuth = (user) =>{
+	componentDidMount(){
+		auth.onAuthStateChanged(user =>{
+			if(user){
+				// User is signed in
+				this.handleAuth(user)
+			} else {
+				// User is signed out
+				this.handleUnauth()
+			}
+		})
+	}
+
+	handleAuth = (oAuthUser) =>{
+		const user = {
+			uid: oAuthUser.uid,
+			displayName: oAuthUser.displayName,
+			email: oAuthUser.email,
+			photoUrl: oAuthUser.photoURL,
+		}
+
 		this.setState({user})
 	}
 
@@ -22,16 +42,23 @@ class App extends Component {
 	}
 
 	signOut = () =>{
+		auth.signOut()
 		this.setState({user: {}})
+		localStorage.removeItem('user')
+	}
+
+	handleUnauth = () =>{
+		this.setState({user: {}})
+		localStorage.removeItem('user')
 	}
 
 	render(){
 		return (
 			<div className="App">{
-					this.signedIn() // ternary
-						? <Main user={this.state.user} signOut={this.signOut}/>
-						: <SignIn handleAuth={this.handleAuth}/>
-				}
+				this.signedIn() // ternary
+					? <Main user={this.state.user} signOut={this.signOut}/>
+					: <SignIn handleAuth={this.handleAuth}/>
+			}
 			</div>
 		)
 	}
