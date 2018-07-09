b5410d8bddbcfd7a175d07749f643e49477fa621
Dante Razo <dante@razonet.org>
2018-07-09 09:52:31 -0400

    Example for importing Firebase

--- src/base.example.js
+++ src/base.example.js
@@ -1,4 +1,8 @@
 import firebase from 'firebase/app'
+import 'firebase/database'
+import 'firebase/auth'
+import Rebase from 're-base'
+
 
 // Initialize Firebase
 const config = {
@@ -11,3 +15,13 @@ const config = {
 }
 
 const app = firebase.initializeApp(config)
+
+// Configure authentication
+export const auth = firebase.auth()
+export const googleProvider = new firebase.auth.GoogleAuthProvider()
+
+// Configure database
+const db = firebase.database(app)
+const base = Rebase.createClass(db)
+
+export default base
