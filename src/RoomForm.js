9275c2eb812b445ae9ae656bc69b85bf4a37cdc9
Dante Razo <dante@razonet.org>
2018-07-10 10:04:41 -0400

    Added form to add rooms (WIP)

---
+++ src/RoomForm.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react'
+
+class RoomForm extends Component {
+	state = {
+		room: {
+			name: "",
+			description: "",
+		},
+	}
+
+	handleChange = ev =>{
+		const roomCopy = {...this.state.room}
+		room[ev.target.name] = ev.target.value
+
+		this.setState({room})
+	}
+
+	render(){
+		return (
+			<div className="RoomForm">
+				<main>
+					<h2>Create a Room</h2>
+					<form>
+						<p>
+							<label htmlFor="name">
+								Room Name
+							</label>
+							<input autoFocus required type="text" name="name"
+								   onChange={this.handleChange}/>
+						</p>
+						<p>
+							<label htmlFor="description">
+								Description
+							</label>
+							<input type="text" name="description"
+								   value={this.state.room.description}
+								   onChange={this.handleChange}/>
+						</p>
+						<div>
+							<button type="submit">
+								Create Room
+							</button>
+						</div>
+					</form>
+				</main>
+			</div>
+		)
+	}
+}
+
+export default RoomForm
